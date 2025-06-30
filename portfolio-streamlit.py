8d690780b79c9d8823ee638269538c7a82250e99
Prahlad-Shukla <heyshukla1@gmail.com>
2025-06-30 21:35:11 +0530

    Update portfolio-streamlit.py

--- portfolio-streamlit.py
+++ portfolio-streamlit.py
@@ -1,2 +1,5 @@
 import streamlit as st
 st.title("welcome to Prahlad's Profile")
+st.header("This is a welcome page in the name or Prahlad Shikla ji")
+st.sidebar.title("Main Menu")
+st.sidebar.header("Explore more about him")
